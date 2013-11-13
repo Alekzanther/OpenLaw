9aa0384d8cce9421254dcc54f2b94c4775dac7d1
Simon900225 <simon900225@gmail.com>
2013-11-13 18:26:39 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo("Holla holla get a dolla")
+    session_start();
+	include 'config.php';
+    $sql = 'SELECT * FROM `user`';
+    $result = mysql_query($sql,$link);
+    while ($row = mysql_fetch_array($result, MYSQL_BOTH)) 
+    {
+        echo("Name: " . $row['name'] . " E-mail: " . $row['email'] . "</br>");
+    }
 ?>
