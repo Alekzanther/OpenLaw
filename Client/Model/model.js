d373c762ca8b6ed30e10cafb57c61cf1e0e1b91e
Simon900225 <simon900225@gmail.com>
2014-05-26 18:26:28 +0200

    Gets data from server and tries to put it in the model

--- Client/Model/model.js
+++ Client/Model/model.js
@@ -1,12 +1,12 @@
 var Model = {
     
-    users: null,
-    articles: null,
-    comments: null,
-    sources: null,
-    tags: null,
-    votes: null,
-    
+    users: Array(),
+    articles: Array(),
+    comments: Array(),
+    sources: Array(),
+    tags: Array(),
+    votes: Array(),
+    test: Array(),
     generateData: function()
     {
     	this.users = Array();
@@ -27,8 +27,26 @@ var Model = {
     
     save: function(param)
     {
-    	alert("a");
-    	alert(param);
+    	alert(this.users[0].name);
+    	alert(param.name);
+    },
+    
+    getRealData: function()
+    {
+	    
+    	this.test = "data";
+    	$.get("http://localhost/OpenLaw/Server/DataAccess/getData.php?type=all", function(data, status) {
+			Model.test = JSON.parse(data);
+			for(items in Model.test)
+			{
+				if (items == "user")
+				{
+					alert(items[1].name);
+				}
+				Model.users.push(new User(user.name, user.email,null,null,null,user.id));
+			}
+		});
     }
 };
+Model.getRealData();
 
--- Client/js/controllers/main.js
+++ Client/js/controllers/main.js
@@ -1,4 +1,3 @@
 angular.module('openLaw.controllers').controller('main', ['$scope', '$http', function($scope, $http){
-  	console.log(Model.users);
-	
+	$scope.Model = Model;
 }]);
