b24cdab6b96ce9affd545b9ca92b9daa5b9588ad
Alekzanther <alexanderlndbrg@gmail.com>
2014-11-04 21:15:40 +0000

    basic article creation

--- client/templates/articles/articles_list.js
+++ client/templates/articles/articles_list.js
@@ -1,20 +1,6 @@
-var articlesData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  }, 
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
 
 Template.articlesList.helpers({
   articles: function() {
-     return Articles.find();
+     return Articles.find({}, {sort: {submitted: -1}});
   }
 });
