c49336107fa9e2ef20df82ea005fcdea1a304031
Alexander Lindberg <alexanderlndbrg@gmail.com>
2015-11-25 23:43:13 +0100

    three different lists on the front page and some styling stuff

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,13 +1,1 @@
-Template.home.onCreated(function() {
-    var self = this;
-    self.autorun(function() {
-      self.subscribe('articles');
-      
-    });
-});
 
-Template.home.helpers({
-  Articles: function() {
-    return Articles.find({});
-  }
-});
