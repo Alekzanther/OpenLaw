9d726bafd8b847113dd6ad495ab0b939aa176c66
Simon Bengtsson <simon900225@gmail.com>
2015-12-23 19:34:12 +0100

    correct vote score on articles when voting. No way to cheat. i think...

--- client/templates/articles/article_page.js
+++ client/templates/articles/article_page.js
@@ -1,16 +1,42 @@
+Template.article_page.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+      self.subscribe('votes');
+    });
+});
+
 Template.article_page.helpers({
-  Article: function() {
-    return Articles.findOne();
-  }
+    Article: function() {
+        return Articles.findOne();
+    },upvoted: function(){
+        vote = Votes.findOne({articleId: this._id});
+
+        if(_.isUndefined(vote) == false){
+            if (vote.value == 1){
+                return "green";
+            }
+        }
+
+        return "";
+    },downvoted: function(){
+        vote = Votes.findOne({articleId: this._id});
+
+        if(_.isUndefined(vote) == false){
+            if(vote.value == -1){
+                return "red";
+            }
+        }
+
+        return "";
+    }
+
 });
 
 Template.article_page.events({
     "click .upvote": function(event, template){
-        console.log(this);
         Meteor.call('voteArticle', {article: this, value: 1});
     },
     "click .downvote": function(event, template){
-        console.log(this);
         Meteor.call('voteArticle', {article: this, value: -1});
     }
 });
